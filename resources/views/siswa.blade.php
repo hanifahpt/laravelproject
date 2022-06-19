30cac9600532dbcdf085ad5721c435b02b59fddd
hanifahpt <106040982+hanifahpt@users.noreply.github.com>
2022-06-20 00:03:33 +0700

    Update siswa.blade.php

--- resources/views/siswa.blade.php
+++ resources/views/siswa.blade.php
@@ -10,16 +10,12 @@ integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolfl
 </head>
 <body>
     <div class="container text-center mt-3 pt-3 ng-white">
-        <h1 class="bg-dark px-3 py-3 text-white d-inline-block">{{$nama}}</h1>
-        <h1 class="bg-dark px-3 py-1 text-white d-inline-block">{{$nilai}</h1>
-        <br>
-        @if (($nilai >= 0) and ($nilai <50))
-        <div class="alert alert-danger d-inline-block">Maaf, Anda Tidak Lulus</div>
-        
-        @elseif (($nilai >= 50) and ($nilai <=100))
-        <div class="alert alert-danger d-inline-block">Selamat, Anda Lulus</div>
-        @endif
-        </div>
-    </div>
+        @if (($nilai >= 0) and ($nilai <50)) 
+<div class="alert alert-danger d-inline-block">Maaf, Anda Tidak Lulus</div>
+@elseif (($nilai >= 50) and($nilai <=100))
+<div class="alert alert-success d-inline-block">Selamat, Anda Lulus</div>
+@else
+<div class="alert alert-dark d-inline-block">Nilai Tidak Valid</div>
+@endif
 </body>
 </html>
